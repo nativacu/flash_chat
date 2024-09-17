7edc7e1dbad504fba3c8b1b00571ba03088bbcef
nativacu <nativacu@gmail.com>
2024-09-16 23:41:45 -0400

    feat: displaying messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,16 @@ import 'package:flash_chat/constants.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+class MessageData {
+  final String message;
+  final String sender;
+
+  MessageData(
+    this.message,
+    this.sender,
+  );
+}
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
   static const String id = 'chat';
@@ -56,8 +66,15 @@ class ChatScreenState extends State<ChatScreen> {
         print(e);
       }
     }
+  }
 
-    // sender , text
+  Future<void> subscribeToMessages() async {
+    var snapshots = _fireStore.collection('messages').snapshots();
+    await for (var snapshot in snapshots) {
+      for (var doc in snapshot.docs) {
+        print(doc.data);
+      }
+    }
   }
 
   @override
@@ -79,6 +96,35 @@ class ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: _fireStore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                List<Widget> textList = [];
+
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+
+                for (var doc in snapshot.data!.docs) {
+                  Map<String, dynamic> data = doc.data();
+                  textList.add(
+                    Text(
+                      '${data['text']} from ${data['sender']}',
+                    ),
+                  );
+                }
+
+                return Expanded(
+                  child: ListView(
+                    children: textList,
+                  ),
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
